6f3e77581a9079442658383f0267782b9bc728d3
Joshua Patterson <jleegippies@gmail.com>
2021-03-10 20:09:18 -0600

    Initial Commit

---
+++ gates/not_gate.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Joshua on 3/10/2021.
+//
+
+#include "not_gate.h"
+
+bool NotGate::operate(bool a) {
+    return !a;
+}
+
---
+++ gates/not_gate.h
@@ -0,0 +1,11 @@
+//
+// Created by Joshua on 3/10/2021.
+//
+
+#ifndef INC_16BITEMULATOR_NOT_GATE_H
+#define INC_16BITEMULATOR_NOT_GATE_H
+class NotGate {
+public:
+    bool operate(bool a);
+};
+#endif //INC_16BITEMULATOR_NOT_GATE_H
