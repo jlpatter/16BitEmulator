0fb5614cdbe5c52d1c6c1060c5ab745294023dc7
Joshua Patterson <jleegippies@gmail.com>
2021-03-12 14:02:50 -0600

    Fixed the multi adder

--- src/arithmetics/MultiAdder.cpp
+++ src/arithmetics/MultiAdder.cpp
@@ -15,15 +15,15 @@ MultiAdder::~MultiAdder() {
 bool* MultiAdder::operate(bool *a, bool *b, bool c) {
     bool* result = new bool[17];
 
-    std::pair<bool, bool> output = fullAdder[0].operate(a[0], b[0], c);
-    result[0] = output.first;
+    std::pair<bool, bool> output = fullAdder[15].operate(a[15], b[15], c);
+    result[16] = output.second;
 
-    for (int i = 1; i < 16; i++) {
-        output = fullAdder[i].operate(a[i], b[i], output.second);
-        result[i] = output.first;
+    for (int i = 14; i >= 0; i--) {
+        output = fullAdder[i].operate(a[i], b[i], output.first);
+        result[i + 1] = output.second;
     }
 
-    result[16] = output.second;
+    result[0] = output.first;
 
     return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     auto *multiAdder = new MultiAdder();
     bool aInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
-    bool bInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
-    bool *result = multiAdder->operate(aInput, bInput, true);
+    bool bInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, true};  // 101
+    bool *result = multiAdder->operate(aInput, bInput, false);
     for (int i = 0; i < 17; i++) {
         std::cout << result[i];
     }
