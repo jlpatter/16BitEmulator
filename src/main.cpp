677e03d0fc019f9f17c1000e546ed012dd38d950
Joshua Patterson <jleegippies@gmail.com>
2021-03-12 12:14:15 -0600

    Minor lint fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     auto *multiAdder = new MultiAdder();
     bool aInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
     bool bInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
-    bool* result = multiAdder->operate(aInput, bInput, true);
+    bool *result = multiAdder->operate(aInput, bInput, true);
     for (int i = 0; i < 17; i++) {
         std::cout << result[i];
     }
