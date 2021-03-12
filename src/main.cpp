ed6b9c40c61f38afb39f970f905d774cd61122e2
Joshua Patterson <jleegippies@gmail.com>
2021-03-12 12:01:30 -0600

    Fixed some memory problems

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
     auto *multiAdder = new MultiAdder();
-    bool aInput[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
-    bool bInput[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
+    bool aInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
+    bool bInput[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true};  // 1
     bool* result = multiAdder->operate(aInput, bInput, true);
     for (int i = 0; i < 17; i++) {
         std::cout << result[i];
     }
     std::cout << std::endl;
     delete multiAdder;
-    delete result;
+    delete[] result;
     return 0;
 }
